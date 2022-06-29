d14dd0a147fe0f17e298cc7ad38d034279ead5ec
Eduardo Albuquerque Ribeiro <eduardoribeiro.advg@gmail.com>
2022-06-28 22:35:09 -0300

    Update Aula-20.06.2022.py

--- Aula-20.06.2022.py
+++ Aula-20.06.2022.py
@@ -50,3 +50,70 @@ print(f"Você digitou {vari2} ímpares")
 print(f"O menor número é {z}")
 print(f"O maior número é {y}")
 """
+
+"""
+i = 0
+while (i < 6):
+    print(i)
+    i += 1
+"""
+"""
+inicio = 0
+
+while(inicio <= 10):
+    
+    if (inicio%3 == 0):
+        print(inicio)
+
+    inicio += 1
+"""
+#Faça um programa para calcular a tabuada:
+#do 1 ao 10 para um número informado pelo usuário.
+#do X ao Y para um número informado pelo usuário
+#(o usuário também deve informar os valores de X e Y).
+"""
+tabu = int(input("Digite número que deseja saber a tabuada: "))
+oito = 1
+while (oito <= 10):
+    ada = tabu*oito
+    print(f"{tabu} X {oito} = {ada}")
+    oito += 1
+"""
+"""
+usu = int(input("Qual tabuada você deseja saber? "))
+inicio = int(input("Qual o inicio do intervalo? "))
+fim = int(input("Qual o final do intervalo? "))
+
+while(inicio <= fim):
+    calc = usu*inicio
+    print(f"{inicio} X {fim} = {calc}")
+    inicio = inicio+1
+"""
+#Na matemática, o fatorial de um número natural n, representado por n!, 
+# é o produto de todos os inteiros positivos menores ou iguais a n. 
+# Por exemplo: o fatorial de 5 é representado por 5! que é igual a 5 x 4 x 3 x 2 x 1. 
+# Faça um programa que peça um número para o usuário
+#  e apresente na tela seu fatorial.
+"""
+
+bora = int(input("Digite o fatorial desejado: "))
+termina = 1
+while(bora > 0):
+    termina = termina*bora
+    print(termina)
+    bora -= 1
+
+"""
+#Sendo H = 1 + 1/2 + 1/3 + 1/4 + ... + 1/N, faça um programa que 
+# peça ao usuário qual o termo final (N) e calcule o valor de H.
+"""
+finicio = 0
+n = int(input("Digite o termo final: "))
+h = 0
+
+while(finicio <= n):
+    if (finicio > 0):
+        h = h + (1/finicio)
+    finicio += 1
+print(h)
+"""
